e559c6212b43e05f4519ce1174ff8788f515068a
Andrey <andrey-199213@mail.ru>
2021-03-09 00:54:55 +0300

    Update GameStatSO.cs

--- Assets/Scripts/GameStatSO.cs
+++ Assets/Scripts/GameStatSO.cs
@@ -6,15 +6,6 @@ public class GameStatSO : ScriptableObject
     public float record;
     public bool tutorialEnding;
 
-
-    [Header("Параметры музыки")]
-    public bool musicPlay;
-    public bool effectsPlay;
-    [Range(0, 1)] public float masterVolume;
-    [Range(0, 1)] public float musicVolume;
-    [Range(0, 1)] public float effectsVolume;
-
-
     public void CheckRecord(float value) => record = Mathf.Max(record, value);
 
     public void EndTutorial() => tutorialEnding = true;
